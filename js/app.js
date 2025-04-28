02ce17a8e11dfefc7a3ce53491a62d81404ab219
Hakan Vardar <hakovardo123@gmail.com>
2025-04-28 17:19:54 +0300

    chore: task input form and added a feature to add todos with enter. Closes #6

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-document.getElementById("addTaskButton").addEventListener("click", () => {
+document.getElementById("addTaskButton").addEventListener("click", addTask);
+document.getElementById("taskInput").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+function addTask() {
     const taskInput = document.getElementById("taskInput");
     const taskDescription = taskInput.value.trim();
 
-    if (taskDescription != "") {
+    if (taskDescription !== "") {
         const taskList = document.getElementById("taskList");
         const listItem = document.createElement("li");
-
         listItem.textContent = taskDescription;
         taskList.appendChild(listItem);
 
+        // Clear input after adding task
         taskInput.value = "";
     } else {
-        alert("Please enter a task description!")
+        alert("Please enter a task description!");
     }
-
-});
+}
